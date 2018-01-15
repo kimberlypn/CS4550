818555ab2482518fc7ee48df44bee507bb48f7ec
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-15 02:32:22 +0000

    Added comments for JS file and set up HTML for Task 3

--- hw02/js/task2.js
+++ hw02/js/task2.js
@@ -1,12 +1,15 @@
+// Makes an "alert" popup with the current value of the counter
 function showVal() {
   alert(document.getElementById('counter').innerHTML);
 }
 
+// Increments the current counter value by 1
 function increment() {
   var current = document.getElementById('counter').innerHTML;
   document.getElementById('counter').innerHTML = Number(current) + 1;
 }
 
+// Appends a new paragraph containing the number to the bottom of the page
 function append() {
   var paragraph = document.createElement('p');
   paragraph.innerHTML = document.getElementById('counter').innerHTML;
