1c4c30d99dd2da42afa1767b9b93888d3ed57745
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-19 16:20:15 +0000

    Moved JS tag to the bottom to avoid loading issues and moved all JS to JS file to pass the validator

--- hw02/js/task3.js
+++ hw02/js/task3.js
@@ -1,3 +1,10 @@
+
+// Set the onclick function of the three link texts
+document.getElementById('lorem-link').onclick = function() { showText('lorem') };
+document.getElementById('bottles-link').onclick = function() { showText('bottles') };
+document.getElementById('third-link').onclick = function() { showText('third') };
+
+
 // Toggles the display of the corresponding text on the right side
 function showText(id) {
   var flag = document.getElementById(id).style.display;
