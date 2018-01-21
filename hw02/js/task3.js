a25a7512f5da10771d484f3048e851b4af19c79f
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-21 02:07:30 +0000

    Changed it so that clicking  the link would replace the text in the right column rather than allowing the user to view multiple texts at once

--- hw02/js/task3.js
+++ hw02/js/task3.js
@@ -1,17 +1,15 @@
-
 // Set the onclick function of the three link texts
 document.getElementById('lorem-link').onclick = function() { showText('lorem') };
 document.getElementById('bottles-link').onclick = function() { showText('bottles') };
 document.getElementById('third-link').onclick = function() { showText('third') };
 
-
-// Toggles the display of the corresponding text on the right side
+// Replaces the text in the right column with the text corresponding to the id
 function showText(id) {
-  var flag = document.getElementById(id).style.display;
-  if (flag === "none" || flag === "") {
-    document.getElementById(id).style.display = "inline";
-  }
-  else {
-    document.getElementById(id).style.display = "none";
+  var texts = ['lorem', 'bottles', 'third'];
+  document.getElementById(id).style.display = "inline";
+  for (var i = 0; i < texts.length; i++) {
+    if (texts[i] != id) {
+      document.getElementById(texts[i]).style.display = "none";
+    }
   }
 }
