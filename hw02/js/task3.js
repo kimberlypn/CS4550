9780637525ee690b33369b9a69a63d117a12e99d
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-21 21:40:16 +0000

    Made Lorem Ipsum text appear on page load per Task 3 instructions and wrapped HTML files to recommended 80 char/line

--- hw02/js/task3.js
+++ hw02/js/task3.js
@@ -3,10 +3,14 @@ document.getElementById('lorem-link').onclick = function() { showText('lorem') }
 document.getElementById('bottles-link').onclick = function() { showText('bottles') };
 document.getElementById('third-link').onclick = function() { showText('third') };
 
+// Make the Lorem Ipsum text visible when the page is first loaded
+document.getElementById('lorem').style.display = "inline";
+
+// Array holding the ids of the available links
+var texts = ['lorem', 'bottles', 'third'];
+
 // Replaces the text in the right column with the text corresponding to the id
 function showText(id) {
-  // Array holding the ids of the available links
-  var texts = ['lorem', 'bottles', 'third'];
   // Display the text corresponding to the id
   document.getElementById(id).style.display = "inline";
   // Hide all of the other texts
