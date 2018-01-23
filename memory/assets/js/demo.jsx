882650e44ca175d7e57580a77a37e14fd0f4b1dd
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-23 11:03:35 -0500

    Click count does not increase if clicked on card that was already matched. There is a bug where two of the same wrong guesses cannot be made in a row

--- memory/assets/js/demo.jsx
+++ memory/assets/js/demo.jsx
@@ -74,7 +74,6 @@ class MemoryGame extends React.Component {
   }
 
   unflip(card) {
-    console.log("hey");
     let xs = _.map(this.state.cards, (c) => {
       if (c.id == card.id || c.id == this.state.prev.id) {
         return _.extend(c, {
@@ -88,6 +87,7 @@ class MemoryGame extends React.Component {
 
     let st1 = _.extend(this.state, {
       flipped: 0,
+      //prev: card,
       cards: xs
     });
     this.setState(st1);
@@ -95,37 +95,40 @@ class MemoryGame extends React.Component {
 
   // Handles what happens when a card is flipped by the user
   flip(card) {
-    //let oldCount = this.state.flipped;
-    // Set the flipped and matched flags of the flipped card
-    let xs = _.map(this.state.cards, (c) => {
-      if (c.id == card.id) {
-        return _.extend(c, {
-          flipped: true,
-          matched: this.matched(this.state.cards, c)
-        });
+    // Only execute if the card has not been matched, the card is the first card
+    // of the game, or if the card is different from the previous card
+    if (!card.matched || (!this.state.prev || this.state.prev.id != card.id)) {
+      // Set the flipped and matched flags of the flipped card
+      let xs = _.map(this.state.cards, (c) => {
+        if (c.id == card.id) {
+          return _.extend(c, {
+            flipped: true,
+            matched: this.matched(this.state.cards, c)
+          });
+        }
+        else {
+          return c;
+        }
+      });
+      // Only update prev if this is the first guess
+      let p = (this.state.flipped == 0) ? card : this.state.prev;
+      // Increment the number of cards flipped
+      let flipped_count = this.state.flipped + 1;
+      // Increment the number of clicks
+      let click_count = (this.state.prev && card.id == this.state.prev.id) ? this.state.clicks : this.state.clicks + 1;
+      // Update and set the new state
+      let st1 = _.extend(this.state, {
+        clicks: click_count,
+        flipped: flipped_count,
+        prev: p,
+        cards: xs,
+      });
+      this.setState(st1);
+      // If this is the second guess, flip back the two cards after 1 second
+      // if they were not a match
+      if (this.state.flipped == 2) {
+        setTimeout(() => {this.unflip(card)}, 1000);
       }
-      else {
-        return c;
-      }
-    });
-    // Only update prev if this is the first guess
-    let p = (this.state.flipped == 0) ? card : this.state.prev;
-    // Increment the number of cards flipped
-    let flipped_count = this.state.flipped + 1;
-    // Increment the number of clicks
-    let click_count = (card.matched) ? this.state.clicks : this.state.clicks + 1;
-    // Update and set the new state
-    let st1 = _.extend(this.state, {
-      clicks: click_count,
-      flipped: flipped_count,
-      prev: p,
-      cards: xs,
-    });
-    this.setState(st1);
-    // If this is the second guess, flip back the two cards after 1 second
-    // if they were not a match
-    if (this.state.flipped == 2) {
-      setTimeout(() => {this.unflip(card)}, 1000);
     }
   }
 
