b2c33085b3e3fffc0ed4f2342eafd4946c6f6c87
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-22 23:24:20 -0500

    Reflip on non-match working but need to set timeout

--- memory/assets/js/demo.jsx
+++ memory/assets/js/demo.jsx
@@ -6,7 +6,7 @@ export default function run_demo(root) {
   ReactDOM.render(<MemoryGame />, root);
 }
 
-// App state for Memory is:
+// App state for MemoryGame is:
 // {
 // matches: int // number of matches so far
 // clicks: int // number of clicks so far
@@ -18,6 +18,7 @@ class MemoryGame extends React.Component {
       matches: 0,
       clicks: 0,
       flipped: 0,
+      prev: null,
       cards: [
         {letter: 'A', matched: false, flipped: false, id: 0},
         {letter: 'A', matched: false, flipped: false, id: 1},
@@ -65,11 +66,36 @@ class MemoryGame extends React.Component {
         return _.extend(c, {matched: true});
       }
     });
+    if (match) {
+      var matches_count = this.state.matches + 1;
+      _.extend(this.state, {matches: matches_count});
+    }
     return match;
   }
 
-  // Increments the number of clicks each time a card is flipped by the user
+  unflip(card) {
+    let xs = _.map(this.state.cards, (c) => {
+      if (c.id == card.id || c.id == this.state.prev.id) {
+        return _.extend(c, {
+          flipped: false
+        });
+      }
+      else {
+        return c;
+      }
+    });
+
+    let st1 = _.extend(this.state, {
+      flipped: 0,
+      cards: xs
+    });
+    this.setState(st1);
+  }
+
+  // Handles what happens when a card is flipped by the user
   flip(card) {
+    //let oldCount = this.state.flipped;
+    // Set the flipped and matched flags of the flipped card
     let xs = _.map(this.state.cards, (c) => {
       if (c.id == card.id) {
         return _.extend(c, {
@@ -81,14 +107,25 @@ class MemoryGame extends React.Component {
         return c;
       }
     });
-    let click_count = this.state.clicks + 1;
+    // Only update prev if this is the first guess
+    let p = (this.state.flipped == 0) ? card : this.state.prev;
+    // Increment the number of cards flipped
     let flipped_count = this.state.flipped + 1;
+    // Increment the number of clicks
+    let click_count = this.state.clicks + 1;
+    // Update and set the new state
     let st1 = _.extend(this.state, {
       clicks: click_count,
       flipped: flipped_count,
+      prev: p,
       cards: xs,
     });
     this.setState(st1);
+    // If this is the second guess, flip back the two cards after 1 second
+    // if they were not a match
+    if (this.state.flipped == 2) {
+      setTimeout(this.unflip(card), 5000000000);
+    }
   }
 
   render() {
@@ -110,6 +147,6 @@ class MemoryGame extends React.Component {
 
 function RenderCards(props) {
   let card = props.card;
-  let text = (card.matched) ? card.letter : '?';
+  let text = (card.matched || card.flipped) ? card.letter : '?';
   return <div className="col-3 text-center" onClick={() => props.flip(card)}><div className="letter">{text}</div></div>
 }
