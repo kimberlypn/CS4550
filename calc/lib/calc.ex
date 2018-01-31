b4eafa69f7d61060f7bd0c389d67ae1fd0a02582
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-01-31 14:02:50 -0500

    Added postfix evaluator

--- calc/lib/calc.ex
+++ calc/lib/calc.ex
@@ -3,6 +3,10 @@ defmodule Calc do
   Documentation for Calc.
   """
 
+  # Converts an expression in infix notation to postfix notation.
+  # expr: expression in infix notation
+  # ops: stack of operators
+  # acc: accumulator for the postfix expression to be returned
   def get_postfix(expr, ops, acc) do
     cond do
       # If there are no more characters
@@ -40,6 +44,7 @@ defmodule Calc do
             (char == "-" && Enum.at(ops, -1) == "*") ||
             (char == "-" && Enum.at(ops, -1) == "/") ->
               get_postfix(expr, Enum.drop(ops, -1), acc ++ Enum.take(ops, -1))
+            # Case where char has higher precedence
             true ->
               get_postfix(rest, ops ++ [char], acc)
           end
@@ -53,12 +58,58 @@ defmodule Calc do
     end
   end
 
+  # Evaluates an expression in postfix notation.
+  # expr: expression in postfix notation to be evaluated
+  # stack: stack of numbers to be evaluated
+  def eval_postfix(expr, stack) do
+    cond do
+      # If there are no more characters, return the stack
+      length(expr) == 0 ->
+        String.to_integer(Enum.at(stack, 0))
+      # If the character is a number, push it to the stack and recurse
+      not Enum.member?(["*","/","+", "-"], Enum.at(expr, 0)) ->
+        rest = Enum.drop(expr, 1)
+        char = Enum.at(expr, 0)
+        eval_postfix(rest, stack ++ [char])
+      # If the character is an operator
+      true ->
+        # Pop the first two numbers on the stack
+        first = String.to_integer(Enum.at(stack, -2))
+        second = String.to_integer(Enum.at(stack, -1))
+        # Add the evaluation of the two numbers to the stack and recurse
+        rest = Enum.drop(expr, 1)
+        case Enum.at(expr, 0) do
+          "*" ->
+            eval_postfix(rest,
+              (stack
+              |> Enum.drop(-1)
+              |> Enum.drop(-1)) ++ [Integer.to_string(first * second)])
+          "/" ->
+          eval_postfix(rest,
+            (stack
+            |> Enum.drop(-1)
+            |> Enum.drop(-1)) ++ [Integer.to_string(first / second)])
+          "+" ->
+          eval_postfix(rest,
+            (stack
+            |> Enum.drop(-1)
+            |> Enum.drop(-1)) ++ [Integer.to_string(first + second)])
+          "-" ->
+          eval_postfix(rest,
+            (stack
+            |> Enum.drop(-1)
+            |> Enum.drop(-1)) ++ [Integer.to_string(first - second)])
+        end
+    end
+  end
+
   def eval(expr) do
-    # Convert the equation from infix to postfix notation
+    # Convert the equation from infix to postfix notation and evaluate it
     expr
     |> String.trim()
     |> String.split()
     |> get_postfix([], [])
+    |> eval_postfix([])
   end
 
   def main() do
