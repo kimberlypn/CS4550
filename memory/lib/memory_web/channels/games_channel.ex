36fd8f486b4ce85d4ce0746556caabc096622027
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-02-11 10:14:28 -0500

    Saving state using Agent works

--- memory/lib/memory_web/channels/games_channel.ex
+++ memory/lib/memory_web/channels/games_channel.ex
@@ -7,15 +7,14 @@ defmodule MemoryWeb.GamesChannel do
     if authorized?(payload) do
       # Get initial game on join
       game = Memory.GameBackup.load(name) || Game.new()
-
-      # Save game after generating new state.
-      Memory.GameBackup.save(socket.assigns[:name], game)
-
+      # Add the game and name to socket assigns
       socket = socket
       |> assign(:game, game)
       |> assign(:name, name)
+      # Send an ok message
       {:ok, %{"join" => name, "game" => Game.client_view(game)}, socket}
     else
+      # Send an unauthorized error message
       {:error, %{reason: "unauthorized"}}
     end
   end
@@ -25,22 +24,37 @@ defmodule MemoryWeb.GamesChannel do
 
   # Sends the clicked card to clicked()
   def handle_in("clicked", %{"card" => c}, socket) do
+    # Call clicked() with the current state and card
     game = Game.clicked(socket.assigns[:game], c)
+    # Update game in socket assigns
     socket = assign(socket, :game, game)
+    # Save game after generating new state
+    Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+    # Send an ok message
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   # Sends a request to unflip the two cards
   def handle_in("unflip", %{}, socket) do
+    # Call unflip() with the current state
     game = Game.unflip(socket.assigns[:game])
+    # Update game in socket assigns
     socket = assign(socket, :game, game)
+    # Save game after generating new state
+    Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+    # Send an ok message
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
   # Sends a reset request
   def handle_in("reset", %{}, socket) do
+    # Call new() to get a fresh state
     game = Game.new()
+    # Update game in socket assigns
     socket = assign(socket, :game, game)
+    # Save game after generating new state
+    Memory.GameBackup.save(socket.assigns[:name], socket.assigns[:game])
+    # Send an ok message
     {:reply, {:ok, %{ "game" => Game.client_view(game)}}, socket}
   end
 
