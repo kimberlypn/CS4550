a74ede19c82c3043f6e241c9769d0f91d2dad893
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-02-15 19:39:23 -0500

    Added login

--- tasks1/lib/tasks1/accounts/accounts.ex
+++ tasks1/lib/tasks1/accounts/accounts.ex
@@ -37,6 +37,14 @@ defmodule Tasks1.Accounts do
   """
   def get_user!(id), do: Repo.get!(User, id)
 
+  # Non-bang variant of get_user()
+  def get_user(id), do: Repo.get(User, id)
+
+  # Returns a user found via email lookup
+  def get_user_by_email(email) do
+    Repo.get_by(User, email: email)
+  end
+
   @doc """
   Creates a user.
 
