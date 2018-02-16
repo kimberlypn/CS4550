0e801b99994ba8df16a15fb129880565c9620e4e
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-02-16 00:11:27 -0500

    Added time validation

--- tasks1/lib/tasks1/assignments/task.ex
+++ tasks1/lib/tasks1/assignments/task.ex
@@ -19,5 +19,16 @@ defmodule Tasks1.Assignments.Task do
     task
     |> cast(attrs, [:title, :description, :completed, :time_spent, :user_id])
     |> validate_required([:title, :description, :completed, :user_id])
+    |> validate_time(:time_spent)
+  end
+
+  # Adapted from https://medium.com/@QuantLayer/writing-custom-validations-for-ecto-changesets-4971881c7684
+  def validate_time(changeset, field, options \\ []) do
+    validate_change(changeset, field, fn _, time ->
+      case rem(time, 15) == 0 do
+        true -> []
+        false -> [{field, options[:message] || "Must be in increments of 15"}]
+      end
+    end)
   end
 end
