f61416c02cf5a91485a2d46b4a8912360b6875c4
Kimberly Nguyen <nguyen.kimb@husky.neu.edu>
2018-04-02 22:42:20 -0400

    Code cleanup

---
+++ tasks3/assets/js/components/no-session.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+// Renders a message telling the user to log in
+export default function NoSession(props) {
+  return (
+    <div id="no-session">
+      <p>Log in to see your tasks.</p>
+    </div>
+  );
+}
--- tasks3/assets/js/components/tasks3.jsx
+++ tasks3/assets/js/components/tasks3.jsx
@@ -4,8 +4,8 @@ import { Provider, connect } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Nav from './nav';
-import Dashboard from './dashboard';
-import TaskForm from './task-form';
+import NoSession from './no-session';
+import Main from './main';
 
 // Adapted from Nat's lecture notes
 export default function tasks3_init(store) {
@@ -19,26 +19,8 @@ export default function tasks3_init(store) {
 
 let Tasks3 = connect((state) => state)((props) => {
   // Choose what to render depending on whether or not the user is logged in
-  var main;
-  if (!props.form.token) {
-    main = (
-      <div id="no-session">
-        <p>Log in to see your tasks.</p>
-      </div>
-    );
-  }
-  else {
-    main = (
-      <div>
-        <Route path="/" exact={true} render={() =>
-          <Dashboard tasks={props.tasks} user={props.form.user_id} />
-        } />
-        <Route path="/tasks" exact={true} render={() =>
-          <TaskForm />
-        } />
-      </div>
-    );
-  }
+  let main = !props.form.token ? <NoSession /> :
+  <Main tasks={props.tasks} user={props.form.user_id} />;
 
   return (
     <Router>
